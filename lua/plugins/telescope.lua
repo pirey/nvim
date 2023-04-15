dbbfe7cf4dbff985af23ce2d0766d38cdd154271
Yeri <yeri@accelbyte.net>
2023-04-16 00:38:01 +0700

    use fugitive

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,9 +1,12 @@
-return {}
--- return {
---   {
---     "nvim-telescope/telescope.nvim",
---     config = function()
---       require("telescope").load_extension("notify")
---     end,
---   },
--- }
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    -- config = function()
+    --   require("telescope").load_extension("notify")
+    -- end,
+    keys = {
+      { "<leader>gs", enabled = false },
+      { "<leader>gc", enabled = false },
+    },
+  },
+}
