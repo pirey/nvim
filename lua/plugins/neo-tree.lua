941bfe5f2218e070660b9e24073d18b59606c8f7
Yeri <yeri@accelbyte.net>
2023-04-17 06:16:18 +0700

    neotree keymap

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,6 +1,28 @@
+local function openRoot()
+  require("neo-tree.command").execute({ toggle = true, dir = require("lazyvim.util").get_root() })
+end
+
+local function openCwd()
+  require("neo-tree.command").execute({ toggle = true, dir = vim.loop.cwd() })
+end
+
 return {
   {
     "nvim-neo-tree/neo-tree.nvim",
+    keys = {
+      {
+        "<leader>fe",
+        openCwd,
+        desc = "Explorer NeoTree (root dir)",
+      },
+      {
+        "<leader>fE",
+        openRoot,
+        desc = "Explorer NeoTree (cwd)",
+      },
+      { "<leader>e", openCwd, desc = "Explorer NeoTree (root dir)" },
+      { "<leader>E", openRoot, desc = "Explorer NeoTree (cwd)" },
+    },
     opts = {
       window = {
         position = "right",
