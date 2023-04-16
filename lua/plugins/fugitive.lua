70c3557164d3d824a344ed4dbdc6cbc74dc54c82
Yeri <yeri@accelbyte.net>
2023-04-17 06:31:12 +0700

    nobuflisted fugitive buffer

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,3 +1,15 @@
+local function augroup(name)
+    return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
+vim.api.nvim_create_autocmd("FileType", {
+    group = augroup("fugitive"),
+    pattern = { "fugitive" },
+    callback = function()
+      vim.opt_local.buflisted = false
+    end,
+})
+
 vim.keymap.set("n", "<leader>gs", "<cmd>G<cr><c-w>o", { desc = "Open git summary" })
 
 return {
