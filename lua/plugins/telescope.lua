7bc867ab231c639cc1216bac9018bd576c5f77c6
Yeri <yeri@accelbyte.net>
2023-04-16 21:48:40 +0700

    truncate telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -11,6 +11,7 @@ return {
         layout_config = { prompt_position = "top" },
         sorting_strategy = "ascending",
         winblend = 0,
+        path_display = { "truncate" },
       },
     },
   },
