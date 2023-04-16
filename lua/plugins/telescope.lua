c9f06cbbe015afa91336a42b2a318521df944487
Yeri <yeri@accelbyte.net>
2023-04-17 06:16:38 +0700

    telescope keymap

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,10 +1,18 @@
+local Util = require("lazyvim.util")
+
 return {
   {
     "nvim-telescope/telescope.nvim",
     keys = {
+      -- git
       { "<leader>gs", false },
       { "<leader>gc", false },
       { "<leader>gS", "<cmd>Telescope git_status<cr>", desc = "Git status with preview" },
+
+      -- find
+      { "<leader><space>", Util.telescope("files", { cwd = false }), desc = "Find Files (cwd)" },
+      { "<leader>ff", Util.telescope("files", { cwd = false }), desc = "Find Files (cwd)" },
+      { "<leader>fF", Util.telescope("files"), desc = "Find Files (root dir)" },
     },
     opts = {
       defaults = {
