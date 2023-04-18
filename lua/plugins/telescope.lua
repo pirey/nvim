fd5819fc197d92a6ab5757c61943ad0abc72785b
Yeri <yeri@accelbyte.net>
2023-04-18 07:44:46 +0700

    telescope lsp_references include declaration

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -55,7 +55,6 @@ return {
       },
       pickers = {
         lsp_references = {
-          include_declaration = false,
           fname_width = 100,
           show_line = false,
           trim_text = true,
