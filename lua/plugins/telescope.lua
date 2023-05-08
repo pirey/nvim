cacd144f66e4112f4a0cb3f2be90b38216a38ca7
Yeri <yeri@accelbyte.net>
2023-05-08 18:31:40 +0700

    toggle telescope preview

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,6 @@
 local Util = require("lazyvim.util")
 local actions = require("telescope.actions")
+local actions_layout = require("telescope.actions.layout")
 local trouble = require("trouble.providers.telescope")
 local transform_mod = require("telescope.actions.mt").transform_mod
 
@@ -68,11 +69,16 @@ return {
         winblend = 0,
         -- path_display = { "truncate" },
         path_display = path_display,
-        preview = false,
+        preview = {
+          hide_on_startup = true,
+        },
         mappings = {
           i = {
             ["<c-c>"] = false,
             ["<esc>"] = actions.close,
+            ["<c-j>"] = actions.move_selection_next,
+            ["<c-k>"] = actions.move_selection_previous,
+            ["<c-p>"] = actions_layout.toggle_preview,
             ["<c-t>"] = trouble.smart_open_with_trouble,
             ["<c-l>"] = custom_actions.open_and_resume,
           },
