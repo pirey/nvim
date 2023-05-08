869f83aa7d4f492b3bc76a060f79fbec1b881a4d
Yeri <yeri@accelbyte.net>
2023-05-08 12:04:17 +0700

    disable paste keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -17,7 +17,7 @@ vim.keymap.set("v", "<c-c>", '"*y')
 vim.keymap.set("n", "gp", "`[v`]", { desc = "Select last pasted text" })
 vim.keymap.set("n", "yp", "yyp")
 vim.keymap.set("n", "yP", "yyP")
-vim.keymap.set("i", "<c-v>", "<c-r>+", { desc = "Paste properly" })
+-- vim.keymap.set("i", "<c-v>", "<c-r>+", { desc = "Paste properly" })
 vim.api.nvim_set_keymap("c", "<c-v>", "<c-r>+", { desc = "Paste" })
 
 -- window
