250cb06f320cad1b367af19cf9323eae6052ae90
Yeri <yeri@accelbyte.net>
2023-05-15 09:09:16 +0700

    neogit opts

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -1,5 +1,15 @@
 return {
   {
-    "TimUntersberger/neogit"
-  }
+    "TimUntersberger/neogit",
+    cmd = "Neogit",
+    dependencies = {
+      "sindrets/diffview.nvim",
+    },
+    opts = {
+      disable_commit_confirmation = true,
+      integrations = {
+        diffview = true,
+      },
+    },
+  },
 }
