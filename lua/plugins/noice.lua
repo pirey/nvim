ad6f23c8b4aa31b4d355df707df34920f528c896
Yeri <arifyeripratama@gmail.com>
2023-05-16 14:25:29 +0700

    configure noice

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "folke/noice.nvim",
+    opts = {
+      views = {
+        cmdline_popup = {
+          border = {
+            style = "none",
+            padding = { 1, 1 },
+          },
+          filter_options = {},
+          win_options = {
+            winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
+          },
+        },
+      },
+    }
+  }
+}
