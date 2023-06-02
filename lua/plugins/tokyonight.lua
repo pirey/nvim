ac3655d412cac2d77f0d2889e27fec37bf67d876
Yeri <yeri@accelbyte.net>
2023-06-02 18:02:02 +0700

    feat: highlight folded

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -1,37 +1,48 @@
+-- make telescope borderless
+local function highlight_telescope(hl, c)
+  local prompt = "#2d3149"
+  hl.TelescopeNormal = {
+    bg = c.bg_dark,
+    fg = c.fg_dark,
+  }
+  hl.TelescopeBorder = {
+    bg = c.bg_dark,
+    fg = c.bg_dark,
+  }
+  hl.TelescopePromptNormal = {
+    bg = prompt,
+  }
+  hl.TelescopePromptBorder = {
+    bg = prompt,
+    fg = prompt,
+  }
+  hl.TelescopePromptTitle = {
+    bg = prompt,
+    fg = prompt,
+  }
+  hl.TelescopePreviewTitle = {
+    bg = c.bg_dark,
+    fg = c.bg_dark,
+  }
+  hl.TelescopeResultsTitle = {
+    bg = c.bg_dark,
+    fg = c.bg_dark,
+  }
+end
+
+local function highlight_fold(hl, c)
+  hl.Folded = {
+    bg = c.bg
+  }
+end
+
 return {
   {
     "folke/tokyonight.nvim",
     opts = {
-      -- make telescope borderless
       on_highlights = function(hl, c)
-        local prompt = "#2d3149"
-        hl.TelescopeNormal = {
-          bg = c.bg_dark,
-          fg = c.fg_dark,
-        }
-        hl.TelescopeBorder = {
-          bg = c.bg_dark,
-          fg = c.bg_dark,
-        }
-        hl.TelescopePromptNormal = {
-          bg = prompt,
-        }
-        hl.TelescopePromptBorder = {
-          bg = prompt,
-          fg = prompt,
-        }
-        hl.TelescopePromptTitle = {
-          bg = prompt,
-          fg = prompt,
-        }
-        hl.TelescopePreviewTitle = {
-          bg = c.bg_dark,
-          fg = c.bg_dark,
-        }
-        hl.TelescopeResultsTitle = {
-          bg = c.bg_dark,
-          fg = c.bg_dark,
-        }
+        highlight_telescope(hl, c)
+        highlight_fold(hl, c)
       end,
     },
   },
