97f9c3eb54f25c07eebb43b6efe3bcd2fdd9af33
Yeri <arifyeripratama@gmail.com>
2023-06-07 21:14:31 +0700

    feat: remove toggleterm plugin

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -39,3 +39,37 @@ vim.opt.formatoptions:remove({ "c", "r", "o" })
 
 -- clean diff
 vim.opt.fillchars:append({ diff = " " })
+
+-- terminal
+if vim.fn.has("win64") == 1 or vim.fn.has("win32") == 1 then
+  local powershell_options = {
+    shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell",
+    shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+    shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+    shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+    shellquote = "",
+    shellxquote = "",
+  }
+
+  for option, value in pairs(powershell_options) do
+    vim.opt[option] = value
+  end
+end
+
+local function is_lazygit_term(bufname)
+  local pattern = "lazygit"
+  return string.match(bufname, pattern)
+end
+
+function _G.handle_term_open()
+  local opts = { buffer = 0 }
+  if is_lazygit_term(vim.fn.bufname()) then
+    return
+  end
+
+  vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+  vim.opt_local.number = false
+  vim.opt_local.relativenumber = false
+end
+
+vim.cmd("autocmd! TermOpen term://* lua handle_term_open()")
