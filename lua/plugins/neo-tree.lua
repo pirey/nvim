b1a5b27bb414834095cb58141a1f52080166594a
Yeri <yeri@accelbyte.net>
2023-06-20 11:28:09 +0700

    feat: disable z keymap neo tree

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -15,6 +15,8 @@ return {
       window = {
         position = "right",
         mappings = {
+          ["z"] = "noop",
+          ["zM"] = "close_all_nodes",
           ["l"] = "open",
           ["h"] = "close_node",
           ["P"] = { "toggle_preview", config = { use_float = false } },
