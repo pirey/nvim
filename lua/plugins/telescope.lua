c695fc283fb7dfcec65fbec797b72cf81b178eea
Yeri <yeri@accelbyte.net>
2023-07-21 08:08:08 +0700

    feat: adjust telescope preview width

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -119,6 +119,7 @@ return {
           prompt_position = "top",
           width = { padding = 0 },
           height = { padding = 0 },
+          preview_width = 0.5,
         },
         sorting_strategy = "ascending",
         winblend = 0,
