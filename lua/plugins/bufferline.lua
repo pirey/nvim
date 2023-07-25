2516f22ef2534b858ccfd961eb8814bc6a25dbb8
Yeri <yeri@accelbyte.net>
2023-07-25 10:49:26 +0700

    feat: bufferline bg highlight

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,4 +1,3 @@
-local colors = require("tokyonight.colors").moon()
 return {
   {
     "akinsho/bufferline.nvim",
@@ -16,32 +15,356 @@ return {
         offsets = {},
         always_show_bufferline = true,
       },
-      -- highlights = {
-      --   fill = {
-      --     bg = colors.bg,
-      --   },
-      --   background = {
-      --     bg = colors.bg,
-      --   },
-      --   buffer_selected = {
-      --     bg = colors.bg_dark,
-      --   },
-      --   buffer_visible = {
-      --     bg = colors.bg,
-      --   },
-      --   close_button = {
-      --     bg = colors.bg,
-      --   },
-      --   close_button_visible = {
-      --     bg = colors.bg,
-      --   },
-      --   close_button_selected = {
-      --     bg = colors.bg_dark,
-      --   },
-      --   separator = {
-      --     bg = colors.bg,
-      --   },
-      -- },
+      highlights = {
+        fill = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        background = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        tab = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        tab_selected = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        tab_separator = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        tab_separator_selected = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        tab_close = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        close_button = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        close_button_visible = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        close_button_selected = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        buffer_visible = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        buffer_selected = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        numbers = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        numbers_visible = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        numbers_selected = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        diagnostic = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        diagnostic_visible = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        diagnostic_selected = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        hint = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        hint_visible = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        hint_selected = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        hint_diagnostic = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        hint_diagnostic_visible = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        hint_diagnostic_selected = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        info = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        info_visible = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        info_selected = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        info_diagnostic = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        info_diagnostic_visible = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        info_diagnostic_selected = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        warning = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        warning_visible = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        warning_selected = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        warning_diagnostic = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        warning_diagnostic_visible = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        warning_diagnostic_selected = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        error = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        error_visible = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        error_selected = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        error_diagnostic = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        error_diagnostic_visible = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        error_diagnostic_selected = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        modified = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        modified_visible = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        modified_selected = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        duplicate_selected = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        duplicate_visible = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        duplicate = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        separator_selected = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        separator_visible = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        separator = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        indicator_visible = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        indicator_selected = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        pick_selected = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        pick_visible = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        pick = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        offset_separator = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+        trunc_marker = {
+          bg = {
+            attribute = "bg",
+            highlight = "Normal",
+          },
+        },
+      },
     },
   },
 }
