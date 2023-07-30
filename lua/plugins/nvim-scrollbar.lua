f6e37c7dc0c19a183d8cf558d3a36129b10011d9
Yeri <yeri@accelbyte.net>
2023-07-30 22:48:51 +0700

    feat: scrollbar

---
+++ lua/plugins/nvim-scrollbar.lua
@@ -0,0 +1,6 @@
+return {
+  {
+    "petertriho/nvim-scrollbar",
+    config = true,
+  },
+}
--- lua/plugins/satellite.lua
+++ lua/plugins/satellite.lua
@@ -1,8 +1,18 @@
--- cool feature but slow, disable it for now
+local function get_minor_version()
+  local v = vim.version()
+  print("minor: " .. v.minor)
+  return v.minor
+end
+
+if true then return {} end
+
+if get_minor_version() < 10 then
+  return {}
+end
+
 return {
   {
     "lewis6991/satellite.nvim",
-    enabled = false,
     config = true,
   }
 }
