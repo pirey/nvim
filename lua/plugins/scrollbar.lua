414fa7d8e1a63afd53929aa4e729c5bbf86ddd42
Yeri <yeri@accelbyte.net>
2023-07-31 14:33:54 +0700

    feat: add gitsigns dependency to scrollbar

--- lua/plugins/scrollbar.lua
+++ lua/plugins/scrollbar.lua
@@ -3,7 +3,7 @@ return {
     "petertriho/nvim-scrollbar",
     dependencies = {
       "kevinhwang91/nvim-hlslens",
-      -- "lewis6991/gitsigns.nvim"
+      "lewis6991/gitsigns.nvim"
     },
     config = function()
       require("scrollbar").setup()
