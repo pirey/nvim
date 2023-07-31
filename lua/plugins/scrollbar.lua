485b51ffe990d3bf9371a254135ba88de56d42d8
Yeri <yeri@accelbyte.net>
2023-07-31 20:06:04 +0700

    feat: change scrollbar highlight

--- lua/plugins/scrollbar.lua
+++ lua/plugins/scrollbar.lua
@@ -3,10 +3,20 @@ return {
     "petertriho/nvim-scrollbar",
     dependencies = {
       "kevinhwang91/nvim-hlslens",
-      "lewis6991/gitsigns.nvim"
+      "lewis6991/gitsigns.nvim",
     },
-    config = function()
-      require("scrollbar").setup()
+    opts = {
+      handle = {
+        text = " ",
+        blend = 30, -- Integer between 0 and 100. 0 for fully opaque and 100 to full transparent. Defaults to 30.
+        color = nil,
+        color_nr = nil, -- cterm
+        highlight = "Visual",
+        hide_if_all_visible = true, -- Hides handle if all lines are visible
+      },
+    },
+    config = function(_, opts)
+      require("scrollbar").setup(opts)
       require("scrollbar.handlers.search").setup({
         override_lens = function() end,
       })
