a163e86ad64badcb0a17ae385093d075bb2c1bff
Yeri <arifyeripratama@gmail.com>
2023-08-13 17:48:45 +0700

    feat: ensure install ocaml treesitter

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -2,6 +2,7 @@ return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
     ignore_install = { "help" },
+    ensure_installed = { "ocaml" },
     highlight = {
       disable = { "markdown", "json" },
     },
