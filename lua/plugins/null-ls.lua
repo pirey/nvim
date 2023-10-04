0fa3ee031d2458761c6cb527d56c523d5409d5ac
Yeri <yeri@accelbyte.net>
2023-10-04 15:19:28 +0700

    fix: typescript error

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -1,3 +1,4 @@
+if true then return {} end
 return {
   {
     "jose-elias-alvarez/null-ls.nvim",
--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -13,34 +13,34 @@ return {
     end,
     opts = {
       autoformat = false,
-      setup = {
-        tsserver = function(_, opts)
-          local util = require("lspconfig.util")
-
-          require("lazyvim.util").on_attach(function(client, buffer)
-            if client.name == "tsserver" then
-              -- stylua: ignore
-              vim.keymap.set("n", "<leader>co", "<cmd>TypescriptOrganizeImports<CR>", { buffer = buffer, desc = "Organize Imports" })
-              -- stylua: ignore
-              vim.keymap.set("n", "<leader>cR", "<cmd>TypescriptRenameFile<CR>", { desc = "Rename File", buffer = buffer })
-            end
-          end)
-
-          opts.server = {
-            root_dir = function(fname)
-              return util.root_pattern(".git")(fname)
-            end,
-          }
-
-          -- if using lspconfig, don't wrap it under server
-          -- require("lspconfig").tsserver.setup({
-          -- root_dir = ...
-          -- })
-
-          require("typescript").setup(opts)
-          return true
-        end,
-      },
+      -- setup = {
+      --   tsserver = function(_, opts)
+      --     local util = require("lspconfig.util")
+      --
+      --     require("lazyvim.util").on_attach(function(client, buffer)
+      --       if client.name == "tsserver" then
+      --         -- stylua: ignore
+      --         vim.keymap.set("n", "<leader>co", "<cmd>TypescriptOrganizeImports<CR>", { buffer = buffer, desc = "Organize Imports" })
+      --         -- stylua: ignore
+      --         vim.keymap.set("n", "<leader>cR", "<cmd>TypescriptRenameFile<CR>", { desc = "Rename File", buffer = buffer })
+      --       end
+      --     end)
+      --
+      --     opts.server = {
+      --       root_dir = function(fname)
+      --         return util.root_pattern(".git")(fname)
+      --       end,
+      --     }
+      --
+      --     -- if using lspconfig, don't wrap it under server
+      --     -- require("lspconfig").tsserver.setup({
+      --     -- root_dir = ...
+      --     -- })
+      --
+      --     require("typescript").setup(opts)
+      --     return true
+      --   end,
+      -- },
       -- servers = {
       --   dartls = {
       --     cmd = { "dart", "language-server", "--protocol=lsp" },
