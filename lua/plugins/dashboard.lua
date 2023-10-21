59d632b07d4418980abc48878d5e959b37afad15
Yeri <arifyeripratama@gmail.com>
2023-10-21 14:32:19 +0700

    chore: update packages

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -46,8 +46,8 @@ return {
           { action = "Git | only",                                                      desc = " Git",             icon = " ", key = "g" },
           { action = [[lua require("lazyvim.util").telescope.config_files()()]], desc = " Config",          icon = " ", key = "c" },
           { action = 'lua require("persistence").load()',                        desc = " Restore Session", icon = " ", key = "s" },
-          { action = "LazyExtras",                                               desc = " Lazy Extras",     icon = " ", key = "x" },
-          { action = "Lazy",                                                     desc = " Lazy",            icon = "󰒲 ", key = "l" },
+          -- { action = "LazyExtras",                                               desc = " Lazy Extras",     icon = " ", key = "x" },
+          -- { action = "Lazy",                                                     desc = " Lazy",            icon = "󰒲 ", key = "l" },
           { action = "qa",                                                       desc = " Quit",            icon = " ", key = "q" },
         },
           footer = function()
