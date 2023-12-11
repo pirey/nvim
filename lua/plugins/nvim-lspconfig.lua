3d3b4231c12bd6f05b69fa68a20c1f720d7ced88
Yeri <arifyeripratama@gmail.com>
2023-12-11 07:01:01 +0700

    feat: setup tailwindcss lsp for blade

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -12,40 +12,11 @@ return {
       keys[#keys + 1] = { "<leader>k", vim.lsp.buf.hover }
     end,
     opts = {
-      -- autoformat = false,
-      -- setup = {
-      --   tsserver = function(_, opts)
-      --     local util = require("lspconfig.util")
-      --
-      --     require("lazyvim.util").on_attach(function(client, buffer)
-      --       if client.name == "tsserver" then
-      --         -- stylua: ignore
-      --         vim.keymap.set("n", "<leader>co", "<cmd>TypescriptOrganizeImports<CR>", { buffer = buffer, desc = "Organize Imports" })
-      --         -- stylua: ignore
-      --         vim.keymap.set("n", "<leader>cR", "<cmd>TypescriptRenameFile<CR>", { desc = "Rename File", buffer = buffer })
-      --       end
-      --     end)
-      --
-      --     opts.server = {
-      --       root_dir = function(fname)
-      --         return util.root_pattern(".git")(fname)
-      --       end,
-      --     }
-      --
-      --     -- if using lspconfig, don't wrap it under server
-      --     -- require("lspconfig").tsserver.setup({
-      --     -- root_dir = ...
-      --     -- })
-      --
-      --     require("typescript").setup(opts)
-      --     return true
-      --   end,
-      -- },
-      -- servers = {
-      --   dartls = {
-      --     cmd = { "dart", "language-server", "--protocol=lsp" },
-      --   },
-      -- },
+      servers = {
+        tailwindcss = {
+          filetypes_include = { "blade" },
+        },
+      },
     },
   },
 }
