fff45138e79a06eb3bfbe7ec6af96de7a8561dc7
Yeri <arifyeripratama@gmail.com>
2023-12-22 17:47:59 +0700

    feat: change org files location

--- lua/plugins/orgmode.lua
+++ lua/plugins/orgmode.lua
@@ -5,8 +5,8 @@ return {
   },
   event = "VeryLazy",
   opts = {
-    org_agenda_files = "~/orgfiles/**/*",
-    org_default_notes_file = "~/orgfiles/refile.org",
+    org_agenda_files = "~/org/**/*",
+    org_default_notes_file = "~/org/refile.org",
     org_todo_keywords = { "TODO", "IN_PROGRESS", "|", "DONE", "CANCELLED" },
     org_startup_folded = "showeverything",
   },
