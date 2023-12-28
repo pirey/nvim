0a86058e89143bb7a91399d5f353a0ba2f5125e6
Yeri <yeri@accelbyte.net>
2023-12-28 07:57:59 +0700

    feat: change mapping mini.surround

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -3,13 +3,13 @@ return {
     "echasnovski/mini.surround",
     opts = {
       mappings = {
-        add = ",sa", -- Add surrounding in Normal and Visual modes
-        delete = ",sd", -- Delete surrounding
-        find = ",sf", -- Find surrounding (to the right)
-        find_left = ",sF", -- Find surrounding (to the left)
-        highlight = ",sh", -- Highlight surrounding
-        replace = ",sr", -- Replace surrounding
-        update_n_lines = ",sn", -- Update `n_lines`
+        add = ",as", -- Add surrounding in Normal and Visual modes
+        delete = ",ds", -- Delete surrounding
+        find = ",fs", -- Find surrounding (to the right)
+        find_left = ",Fs", -- Find surrounding (to the left)
+        highlight = ",hs", -- Highlight surrounding
+        replace = ",cs", -- Replace surrounding
+        update_n_lines = ",ns", -- Update `n_lines`
       },
     },
   },
