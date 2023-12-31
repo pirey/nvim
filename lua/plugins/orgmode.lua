18f56c38f34729b92fa375d0cbfcb3643530c02c
Yeri <arifyeripratama@gmail.com>
2024-01-01 02:14:21 +0700

    feat: orgmode bullet

--- lua/plugins/orgmode.lua
+++ lua/plugins/orgmode.lua
@@ -1,13 +1,20 @@
 return {
-  "nvim-orgmode/orgmode",
-  dependencies = {
-    { "nvim-treesitter/nvim-treesitter", lazy = true },
+  {
+    "nvim-orgmode/orgmode",
+    dependencies = {
+      { "nvim-treesitter/nvim-treesitter", lazy = true },
+    },
+    event = "VeryLazy",
+    opts = {
+      org_agenda_files = "~/org/**/*",
+      org_default_notes_file = "~/org/refile.org",
+      org_todo_keywords = { "TODO", "IN_PROGRESS", "|", "DONE", "CANCELLED" },
+      org_startup_folded = "showeverything",
+    },
   },
-  event = "VeryLazy",
-  opts = {
-    org_agenda_files = "~/org/**/*",
-    org_default_notes_file = "~/org/refile.org",
-    org_todo_keywords = { "TODO", "IN_PROGRESS", "|", "DONE", "CANCELLED" },
-    org_startup_folded = "showeverything",
+  {
+    "akinsho/org-bullets.nvim",
+    dependencies = { "nvim-orgmode/orgmode" },
+    opts = {},
   },
 }
