927a85b62e550c2c5023b53a4b59ca01e88934f4
Yeri <yeri@accelbyte.net>
2024-01-02 11:12:30 +0700

    feat: enable headlines.nvim

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,4 +1,15 @@
 return {
   "lukas-reineke/headlines.nvim",
-  enabled = false,
+  opts = {
+    markdown = {
+      fat_headline_lower_string = "▀",
+    },
+    norg = {
+      fat_headline_lower_string = "▀",
+    },
+    org = {
+      fat_headline_lower_string = "▀",
+    },
+  },
+  -- enabled = false,
 }
