30a0659a621409a8bd6177cda5145490804f7a40
Yeri <yeri@accelbyte.net>
2024-01-24 07:26:16 +0700

    feat: keymap to toggle diagnostic

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,5 +1,14 @@
-vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>",
-    {silent = true, noremap = true}
-)
+vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>", { silent = true, noremap = true })
+vim.keymap.set("n", "<leader>xd", function()
+  if vim.diagnostic.is_disabled() then
+    vim.diagnostic.enable()
+  else
+    vim.diagnostic.disable()
+  end
+end, {
+  silent = true,
+  noremap = true,
+  desc = "Toggle diagnostic",
+})
 
 return {}
