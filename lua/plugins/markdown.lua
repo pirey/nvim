15f984535c1ce3c151f9c744ba25cf17a4875a97
Yeri <yeri@accelbyte.net>
2024-03-21 06:53:23 +0700

    disable markdown linter

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,16 +1,25 @@
 return {
-  "lukas-reineke/headlines.nvim",
-  enabled = false, -- disable for slow performance
-  opts = {
-    markdown = {
-      fat_headline_lower_string = "▀",
-    },
-    norg = {
-      fat_headline_lower_string = "▀",
-    },
-    org = {
-      fat_headline_lower_string = "▀",
+  {
+    "lukas-reineke/headlines.nvim",
+    enabled = false, -- disable because of slow performance
+    opts = {
+      markdown = {
+        fat_headline_lower_string = "▀",
+      },
+      norg = {
+        fat_headline_lower_string = "▀",
+      },
+      org = {
+        fat_headline_lower_string = "▀",
+      },
     },
   },
-  -- enabled = false,
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters_by_ft = {
+        markdown = {},
+      },
+    },
+  },
 }
