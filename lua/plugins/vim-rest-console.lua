6f001393be3e30db3e7f5aa59494537800766521
Yeri <yeri@accelbyte.net>
2024-04-25 13:37:09 +0700

    adjust vrc

--- lua/plugins/vim-rest-console.lua
+++ lua/plugins/vim-rest-console.lua
@@ -2,12 +2,16 @@ return {
   "diepm/vim-rest-console",
   init = function()
     os.execute("mkdir -p ~/.vim-rest-console")
-    vim.api.nvim_create_user_command("VRC", function(arg)
+    local function handle_open_console(arg)
       local filename = arg.args
       if #filename == 0 then
         filename = "index"
       end
       vim.cmd("edit ~/.vim-rest-console/" .. filename .. ".rest")
-    end, { nargs = "?", desc = "Open http client" })
+    end
+    local opts = { nargs = "?", desc = "Open HTTP Console" }
+    vim.api.nvim_create_user_command("VRC", handle_open_console, opts)
+    vim.api.nvim_create_user_command("Http", handle_open_console, opts)
+    vim.cmd("autocmd! BufRead,BufNewFile __REST_response__ setlocal ft=json")
   end,
 }
