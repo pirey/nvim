ec783d575deffa757c48a379cbf859ccff5b9ff8
Yeri <yeri@accelbyte.net>
2024-04-30 10:25:59 +0700

    update vrc config

--- lua/plugins/vim-rest-console.lua
+++ lua/plugins/vim-rest-console.lua
@@ -16,7 +16,7 @@ return {
 
     -- vim.g.vrc_set_default_mapping = 0
     vim.g.vrc_response_default_content_type = "application/json"
-    vim.g.vrc_output_buffer_name = "VRC_OUTPUT.json"
+    -- vim.g.vrc_output_buffer_name = "VRC_OUTPUT.json"
     vim.g.vrc_auto_format_response_patterns = {
       json = "jq",
     }
