4bb10b828d030e9bac7eeeec0f390e4e0232a418
Yeri <yeri@accelbyte.net>
2024-05-15 02:08:00 +0700

    todo customize iceberg

--- lua/plugins/iceberg.lua
+++ lua/plugins/iceberg.lua
@@ -1,6 +1,18 @@
 return {
   "cocopon/iceberg.vim",
-  lazy = true,
+  init = function()
+    local custom_highlight = vim.api.nvim_create_augroup("CustomIceberg", {})
+    vim.api.nvim_create_autocmd("ColorScheme", {
+      pattern = "iceberg",
+      callback = function()
+        -- #272c42
+        vim.api.nvim_set_hl(0, "CursorLineNr", { bg = "#161821" })
+        vim.api.nvim_set_hl(0, "LineNr", { bg = "#161821" })
+        vim.api.nvim_set_hl(0, "NonText", { fg = "#161821" })
+      end,
+      group = custom_highlight,
+    })
+  end,
 }
 -- return {
 --   "oahlen/iceberg.nvim",
