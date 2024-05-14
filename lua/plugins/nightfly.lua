4961f26e9c15eea1c4e0cbef26290e95ec6366c0
Yeri <yeri@accelbyte.net>
2024-05-14 21:45:48 +0700

    nightfly update statusline highlight

--- lua/plugins/nightfly.lua
+++ lua/plugins/nightfly.lua
@@ -4,17 +4,27 @@ return {
   lazy = false,
   priority = 1000,
   config = function()
+    -- customize highlighting
     local colors = require("nightfly").palette
     local custom_highlight = vim.api.nvim_create_augroup("CustomNightfly", {})
     vim.api.nvim_create_autocmd("ColorScheme", {
       pattern = "nightfly",
       callback = function()
         vim.api.nvim_set_hl(0, "NonText", { fg = colors.bg, bg = "NONE" })
+        vim.api.nvim_set_hl(0, "StatusLine", { fg = colors.fg, bg = colors.deep_blue })
+        vim.api.nvim_set_hl(0, "StatusLineNC", { fg = colors.fg, bg = colors.black_blue })
+        vim.api.nvim_set_hl(0, "IlluminatedWordRead", { bg = colors.dark_blue })
+        vim.api.nvim_set_hl(0, "IlluminatedWordText", { bg = colors.dark_blue })
+        vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { bg = colors.dark_blue })
       end,
       group = custom_highlight,
     })
+
+    -- global options
     vim.g.nightflyWinSeparator = 2
     vim.g.nightflyUnderlineMatchParen = false
+
+    -- activate colorscheme
     vim.cmd.colorscheme("nightfly")
   end,
 }
