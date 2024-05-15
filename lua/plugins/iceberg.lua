11b80a24cd43ff300db66040b9f732724af76de0
Yeri <yeri@accelbyte.net>
2024-05-15 11:56:10 +0700

    customize iceberg

--- lua/plugins/iceberg.lua
+++ lua/plugins/iceberg.lua
@@ -12,16 +12,16 @@ return {
     vim.api.nvim_create_autocmd("ColorScheme", {
       pattern = "iceberg",
       callback = function()
-        -- #272c42
         local bg = "#161821"
+        local visual = "#272c42"
         vim.api.nvim_set_hl(0, "CursorLineNr", { bg = bg })
         vim.api.nvim_set_hl(0, "LineNr", { bg = bg })
         vim.api.nvim_set_hl(0, "Folded", { bg = bg })
         vim.api.nvim_set_hl(0, "NonText", { fg = bg })
         vim.api.nvim_set_hl(0, "EndOfBuffer", { fg = bg, bg = bg })
-        vim.api.nvim_set_hl(0, "IlluminatedWordRead", { bg = "#272c42" })
-        vim.api.nvim_set_hl(0, "IlluminatedWordText", { bg = "#272c42" })
-        vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { bg = "#272c42" })
+        vim.api.nvim_set_hl(0, "IlluminatedWordRead", { bg = visual })
+        vim.api.nvim_set_hl(0, "IlluminatedWordText", { bg = visual })
+        vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { bg = visual })
         vim.api.nvim_set_hl(0, "DiffAdd", { bg = "#45493e", fg = "NONE" })
         vim.api.nvim_set_hl(0, "DiffChange", { bg = "#384851", fg = "NONE" })
         vim.api.nvim_set_hl(0, "DiffDelete", { bg = "#53343b", fg = "NONE" })
