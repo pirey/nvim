1fc31a767b9172956b9b876700d8e1dabcda0f20
Yeri <yeri@accelbyte.net>
2024-05-17 05:45:35 +0700

    iceberg notify highlight

--- lua/plugins/iceberg.lua
+++ lua/plugins/iceberg.lua
@@ -13,6 +13,8 @@ return {
       pattern = "iceberg",
       callback = function()
         local bg = "#161821"
+        local bg_dark = "#3e445e" -- from StatuslineNC
+        local fg = "#c6c8d1"
         local visual = "#272c42"
         vim.api.nvim_set_hl(0, "CursorLineNr", { bg = bg, bold = true })
         vim.api.nvim_set_hl(0, "LineNr", { bg = bg, fg = "#444b71" })
@@ -34,8 +36,31 @@ return {
         patch_hl("DiagnosticSignOk", { bg = bg })
         patch_hl("DiagnosticSignWarn", { bg = bg })
         patch_hl("DiagnosticSignHint", { bg = bg })
-        vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#3e445e" })
+        vim.api.nvim_set_hl(0, "WinSeparator", { fg = bg_dark })
         vim.api.nvim_set_hl(0, "MiniIndentscopeSymbol", { fg = "#3e445e" })
+
+        -- Notify
+        vim.api.nvim_set_hl(0, "NotifyBackground", { fg = fg, bg = bg })
+        vim.api.nvim_set_hl(0, "NotifyERRORBorder", { fg = bg_dark, bg = bg })
+        vim.api.nvim_set_hl(0, "NotifyWARNBorder", { fg = bg_dark, bg = bg })
+        vim.api.nvim_set_hl(0, "NotifyINFOBorder", { fg = bg_dark, bg = bg })
+        vim.api.nvim_set_hl(0, "NotifyDEBUGBorder", { fg = bg_dark, bg = bg })
+        vim.api.nvim_set_hl(0, "NotifyTRACEBorder", { fg = bg_dark, bg = bg })
+        vim.api.nvim_set_hl(0, "NotifyERRORIcon", { fg = fg })
+        vim.api.nvim_set_hl(0, "NotifyWARNIcon", { fg = fg })
+        vim.api.nvim_set_hl(0, "NotifyINFOIcon", { fg = fg })
+        vim.api.nvim_set_hl(0, "NotifyDEBUGIcon", { fg = fg })
+        vim.api.nvim_set_hl(0, "NotifyTRACEIcon", { fg = fg })
+        vim.api.nvim_set_hl(0, "NotifyERRORTitle", { fg = fg })
+        vim.api.nvim_set_hl(0, "NotifyWARNTitle", { fg = fg })
+        vim.api.nvim_set_hl(0, "NotifyINFOTitle", { fg = fg })
+        vim.api.nvim_set_hl(0, "NotifyDEBUGTitle", { fg = fg })
+        vim.api.nvim_set_hl(0, "NotifyTRACETitle", { fg = fg })
+        vim.api.nvim_set_hl(0, "NotifyERRORBody", { fg = fg, bg = bg })
+        vim.api.nvim_set_hl(0, "NotifyWARNBody", { fg = fg, bg = bg })
+        vim.api.nvim_set_hl(0, "NotifyINFOBody", { fg = fg, bg = bg })
+        vim.api.nvim_set_hl(0, "NotifyDEBUGBody", { fg = fg, bg = bg })
+        vim.api.nvim_set_hl(0, "NotifyTRACEBody", { fg = fg, bg = bg })
       end,
       group = custom_highlight,
     })
