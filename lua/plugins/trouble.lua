d4ddb6715007669f6eb06ee68eaf4461a1dd4665
Yeri <yeri@accelbyte.net>
2024-05-18 17:07:32 +0700

    delete unused keymap

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,14 +1,6 @@
-vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>", { silent = true, noremap = true })
-vim.keymap.set("n", "<leader>xd", function()
-  if vim.diagnostic.is_enabled() then
-    vim.diagnostic.enable(false)
-  else
-    vim.diagnostic.enable(true)
-  end
-end, {
-  silent = true,
-  noremap = true,
-  desc = "Toggle diagnostic",
-})
-
-return {}
+return {
+  "folke/trouble.nvim",
+  init = function()
+    vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>", { silent = true, noremap = true })
+  end,
+}
