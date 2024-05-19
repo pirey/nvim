ed7cb33376a61c49ae66ef0f3ed4e632f2a0cfd1
Yeri <yeri@accelbyte.net>
2024-05-19 23:05:19 +0700

    trouble.nvim height half screen

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -3,4 +3,7 @@ return {
   init = function()
     vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>", { silent = true, noremap = true })
   end,
+  opts = {
+    height = vim.fn.winheight(0) / 2,
+  },
 }
