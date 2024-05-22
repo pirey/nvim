a834cda1a27495b848a6ee001e0950e21bc44d45
Yeri <yeri@accelbyte.net>
2024-05-22 11:53:51 +0700

    update treesitter config

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,34 +1,41 @@
-local function setup_blade()
-  local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-  parser_config.blade = {
-    install_info = {
-      url = "https://github.com/EmranMR/tree-sitter-blade",
-      files = { "src/parser.c" },
-      branch = "main",
-    },
-    filetype = "blade",
-  }
+-- local function setup_blade()
+--   local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+--   parser_config.blade = {
+--     install_info = {
+--       url = "https://github.com/EmranMR/tree-sitter-blade",
+--       files = { "src/parser.c" },
+--       branch = "main",
+--     },
+--     filetype = "blade",
+--   }
+--
+--   vim.filetype.add({
+--     pattern = {
+--       [".*%.blade%.php"] = "blade",
+--     },
+--   })
+-- end
 
-  vim.filetype.add({
-    pattern = {
-      [".*%.blade%.php"] = "blade",
-    },
-  })
-end
 return {
-  "nvim-treesitter/nvim-treesitter",
-  config = function()
-    setup_blade()
-    local opts = {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
       ignore_install = { "help" },
       ensure_installed = { "http", "json" },
-      highlight = {
+      highlight = { enable = true },
+      indent = { enable = true },
+      incremental_selection = {
         enable = true,
+        keymaps = {
+          init_selection = "<leader>v",
+          node_incremental = "<leader>v",
+          node_decremental = "<c-h>", -- easier to reach than backspace key
+        },
       },
-      indent = {
-        enable = true,
-      },
-    }
-    require("nvim-treesitter.configs").setup(opts)
-  end,
+    },
+    config = function(_, opts)
+      -- setup_blade()
+      require("nvim-treesitter.configs").setup(opts)
+    end,
+  },
 }
