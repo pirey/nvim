856974fa95b6d5e10653caa4afa725c505620a51
Yeri <yeri@accelbyte.net>
2024-06-26 21:16:41 +0700

    add org todo keyword

--- lua/plugins/orgmode.lua
+++ lua/plugins/orgmode.lua
@@ -7,7 +7,7 @@ return {
       require("orgmode").setup({
         org_agenda_files = "~/org/**/*",
         org_default_notes_file = "~/org/refile.org",
-        org_todo_keywords = { "TODO", "IN_PROGRESS", "|", "DONE", "CANCELLED" },
+        org_todo_keywords = { "BLOCKED", "TODO", "IN_PROGRESS", "|", "DONE", "CANCELLED" },
         org_startup_folded = "showeverything",
       })
 
