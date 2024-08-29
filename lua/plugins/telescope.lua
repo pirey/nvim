961a44856a7779c09c1c08cdff92bb5df04ec1e5
Yeri <yeri@accelbyte.net>
2024-08-30 06:55:05 +0700

    disable telescope fzy native plugin

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -74,12 +74,6 @@ return {
       end,
     })
   end,
-  dependencies = {
-    "nvim-telescope/telescope-fzy-native.nvim",
-    config = function()
-      require("telescope").load_extension("fzy_native")
-    end,
-  },
   keys = {
     -- git
     { "<leader>gs", false },
