1d718a7acba289192da4a49f8e066282441b8447
Yeri <yeri@accelbyte.net>
2024-10-14 00:21:37 +0700

    tmp ignore vtsls err msg

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -17,6 +17,16 @@ return {
           merge = false,
         },
       },
+      -- REMOVE THIS once this issue is fixed: https://github.com/yioneko/vtsls/issues/159
+      routes = {
+        {
+          filter = {
+            event = "notify",
+            find = "Request textDocument/documentHighlight failed",
+          },
+          opts = { skip = true },
+        },
+      },
     },
   },
 }
