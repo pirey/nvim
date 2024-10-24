be71c93118392b20bf3b67a0f84252bb58e95254
Yeri <yeri@accelbyte.net>
2024-10-24 09:03:17 +0700

    add astro

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -19,6 +19,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.ui.mini-indentscope" },
     { import = "lazyvim.plugins.extras.ui.treesitter-context" },
+    { import = "lazyvim.plugins.extras.lang.astro" },
     -- { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
