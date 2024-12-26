c6c93a1408698f6477593c2e390bff86fb0c4690
Yeri <mail@yeripratama.com>
2024-12-26 18:08:56 +0700

    reorder orgmode todo keywords

--- lua/plugins/orgmode.lua
+++ lua/plugins/orgmode.lua
@@ -8,7 +8,7 @@ return {
       require("orgmode").setup({
         org_agenda_files = "~/org/**/*",
         org_default_notes_file = "~/org/refile.org",
-        org_todo_keywords = { "TODO", "IN_PROGRESS", "WAITING", "|", "DONE", "INVALID" },
+        org_todo_keywords = { "TODO", "WAITING", "IN_PROGRESS", "|", "DONE", "INVALID" },
         org_startup_folded = "showeverything",
       })
 
