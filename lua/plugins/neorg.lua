a2825eb5a4c260a541c1eebbde3bcde57286c3d4
Yeri <mail@yeripratama.com>
2024-12-31 09:31:23 +0700

    neorg

---
+++ lua/plugins/neorg.lua
@@ -0,0 +1,17 @@
+if true then
+  return {}
+end
+return {
+  {
+    "nvim-neorg/neorg",
+    lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+    version = "*", -- Pin Neorg to the latest stable release
+    -- config = true,
+    opts = {
+      load = {
+        ["core.defaults"] = {},
+        ["core.concealer"] = {},
+      },
+    },
+  },
+}
