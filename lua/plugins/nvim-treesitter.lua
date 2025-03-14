bc87b6209a7691ca78ca7d5a9b5ea48815342222
Yeri <mail@yeripratama.com>
2025-03-15 05:36:20 +0700

    extra php

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,21 +1,3 @@
-local function setup_blade()
-  local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-  parser_config.blade = {
-    install_info = {
-      url = "https://github.com/EmranMR/tree-sitter-blade",
-      files = { "src/parser.c" },
-      branch = "main",
-    },
-    filetype = "blade",
-  }
-
-  vim.filetype.add({
-    pattern = {
-      [".*%.blade%.php"] = "blade",
-    },
-  })
-end
-
 return {
   {
     "nvim-treesitter/nvim-treesitter",
@@ -34,7 +16,6 @@ return {
       },
     },
     config = function(_, opts)
-      setup_blade()
       require("nvim-treesitter.configs").setup(opts)
     end,
   },
