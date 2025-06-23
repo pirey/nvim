a0f9f4dd013441414546606ab15f127620c54d29
Yeri <mail@yeripratama.com>
2025-06-23 14:04:07 +0700

    feat: dadbod

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -213,8 +213,24 @@ require("lazy").setup({
           -- same as ctrl+/
           ['<C-_>'] = { "show" },
         },
-        cmdline = { enabled = false }
+        cmdline = { enabled = false },
+        sources = {
+          per_filetype = {
+            sql = { 'snippets', 'dadbod', 'buffer' },
+          },
+          providers = {
+            dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
+          },
+        },
       },
+    },
+    {
+      "kristijanhusak/vim-dadbod-ui",
+      dependencies = {
+        { "tpope/vim-dadbod", lazy = true },
+        { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+      },
+      cmd = { "DBUI" },
     }
   },
 })
