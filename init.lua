910ba7e5a9688f52c82dc8adb03ffdd24f7da72b
Yeri <mail@yeripratama.com>
2025-07-04 03:55:02 +0700

    feat: highlight tabline

--- init.lua
+++ init.lua
@@ -12,7 +12,7 @@ vim.opt.foldmethod = "indent"
 vim.opt.foldlevelstart = 99
 vim.opt.winborder = "single"
 vim.opt.fillchars:append { diff = " " }
-vim.opt.tabline = "%=Tabs: %{tabpagenr()}/%{tabpagenr('$')}%="
+vim.opt.tabline = "%#TabLineSel#%=Tabs: %{tabpagenr()}/%{tabpagenr('$')}%="
 
 -- ignore .git by default so we doesn't need to specify it when using --hidden
 vim.opt.grepprg = "rg --vimgrep --smart-case --glob '!.git'"
