f934a6cdb414b6f548fc214e2fb9122a5be486fb
Yeri <mail@yeripratama.com>
2025-07-12 13:10:11 +0700

    feat: install github theme

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -381,9 +381,29 @@ require("lazy").setup({
     {
       "lifepillar/vim-solarized8",
       init = function ()
-        vim.opt.background = "light"
-        vim.cmd.colorscheme("solarized8_flat")
+        -- vim.opt.background = "light"
+        -- vim.opt.cursorline = true
+        -- vim.opt.number = true
+        -- vim.cmd.colorscheme("solarized8")
       end
+    },
+    {
+      'projekt0n/github-nvim-theme',
+      name = 'github-theme',
+      config = function ()
+        require("github-theme").setup({
+          groups = {
+            all = {
+              StatusLine = { link = "TabLine" },
+              StatusLineNC = { link = "TabLineFill" }
+            }
+          }
+        })
+        vim.opt.background = "light"
+        vim.opt.cursorline = true
+        vim.opt.number = true
+        vim.cmd.colorscheme("github_light")
+      end,
     },
   }, -- spec
 })
