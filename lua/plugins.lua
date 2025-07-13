86ac1f928c2d5c2d4476916915af2cf819b9fb65
Yeri <mail@yeripratama.com>
2025-07-13 07:50:15 +0700

    feat: tweak iceberg colors

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -235,13 +235,16 @@ require("lazy").setup({
             local diff_add = "#45493e"
             local diff_change = visual
             local diff_delete = "#53343b"
+            local tabline_fg = statusline_fg
+            local tabline_bg = statusline_bg
             local diff_text = "#384851"
+            local border_fg = fg_dark
 
             if vim.o.background == "light" then
               bg = "#e8e9ec"
               fg = "#33374c"
-              fg_dark = fg
-              bg_dark = bg
+              fg_dark = "#cad0de"
+              bg_dark = "#8b98b6"
               statusline_fg = "#e8e9ec"
               statusline_bg = "#757ca3"
               float_bg = "#cad0de"
@@ -251,15 +254,18 @@ require("lazy").setup({
               diff_change = "#ced9e1"
               diff_delete = "#e3d2da"
               diff_text = "#acc5d3"
+              tabline_fg= "#8b98b6"
+              tabline_bg= "#cad0de"
+              border_fg = fg_dark
             end
 
             vim.api.nvim_set_hl(0, "NonText", { link = "Comment" })
             vim.api.nvim_set_hl(0, "EndOfBuffer", { fg = bg, bg = bg }) -- squiggly ~
-            vim.api.nvim_set_hl(0, "WinSeparator", { fg = fg_dark, bold = true })
+            vim.api.nvim_set_hl(0, "WinSeparator", { fg = border_fg, bold = true })
             vim.api.nvim_set_hl(0, "SignColumn", { bg = linenr_bg })
             vim.api.nvim_set_hl(0, "FoldColumn", { bg = bg, fg = fg_dark })
             vim.api.nvim_set_hl(0, "StatusLine", { fg = statusline_fg, bg = statusline_bg })
-            vim.api.nvim_set_hl(0, "TabLineFill", { fg = statusline_fg, bg = statusline_bg })
+            vim.api.nvim_set_hl(0, "TabLineFill", { fg = tabline_fg, bg = tabline_bg })
 
             -- line number
             vim.api.nvim_set_hl(0, "CursorLineNr", { bg = bg, bold = true })
@@ -285,6 +291,7 @@ require("lazy").setup({
             vim.api.nvim_set_hl(0, "FzfLuaBufFlagCur", { link = "Title" })
             vim.api.nvim_set_hl(0, "FzfLuaHeaderText", { link = "Title" })
             vim.api.nvim_set_hl(0, "FzfLuaPathLineNr", { link = "Title" })
+            vim.api.nvim_set_hl(0, "FzfLuaBorder", { fg = border_fg })
 
             patch_group_pattern("DiagnosticUnderline", { undercurl = true })
 
