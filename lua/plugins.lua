0969b81bf9a68446d3e25402cb42dadc0afb101b
Yeri <mail@yeripratama.com>
2025-08-13 21:43:40 +0700

    feat: fff new keymap

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -196,7 +196,7 @@ require("lazy").setup({
       },
       keys = {
         {
-          "ff", -- try it if you didn't it is a banger keybinding for a picker
+          "<leader><leader>f",
           function()
             require("fff").find_files() -- or find_in_git_root() if you only want git files
           end,
