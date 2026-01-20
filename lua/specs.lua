90eafa50b88701e8177cf71a7e2c5722976daa38
Yeri <mail@yeripratama.com>
2026-01-20 20:21:37 +0700

    feat: temp workaround colorizer deprecated function

--- lua/specs.lua
+++ lua/specs.lua
@@ -464,7 +464,12 @@ local colorizer = {
   src = "norcalli/nvim-colorizer.lua",
   config = function()
     vim.o.termguicolors = true
+    local old_deprecate = vim.deprecate
+    ---@diagnostic disable-next-line: duplicate-set-field
+    vim.deprecate = function() end
     require("colorizer").setup()
+    ---@diagnostic disable-next-line: duplicate-set-field
+    vim.deprecate = old_deprecate
   end,
 }
 local dadbod_ui = {
